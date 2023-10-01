d88f4dd11adc712c3f2379d579172a1244dd48c6
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-10-01 06:32:14 +0200

    executing datafinalize by taping commend 'node datafinalize' created const data in execute js. Each new object will be added separately with comment at the bottom new creation date

--- execute/datafinalize.js
+++ execute/datafinalize.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const { newdb } = require("../execute/data.js");
 const { transformedDbvd } = require("../execute/dbvd.js");
 
@@ -16,6 +18,21 @@ for (const key in newdb) {
 }
 console.log(mergedData);
 
-module.exports = {
+const currentDate = new Date();
+const executionDate = currentDate.toISOString();
+
+// Zapisz wynik wraz z datą uruchomienia do pliku
+const outputPath = "../execute/execute.js";
+const outputContent = `const data = ${JSON.stringify(
   mergedData,
-};
+  null,
+  2
+)};\n\n// Nowe dane zostały dodane do pliku: ${executionDate}\n\n`;
+
+fs.appendFile(outputPath, outputContent, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`Nowe dane zostały dodane do pliku: ${outputPath}`);
+});
